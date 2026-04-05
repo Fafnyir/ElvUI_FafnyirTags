eb61fcb134abb913d8dce46b90f730fff467b6dd
Fafnyir <frank@frankpatten.ca>
2026-04-05 09:09:15 -0400

    Full Send

---
+++ FafnyirTags.lua
@@ -0,0 +1,11 @@
+local E, L, V, P, G = unpack(ElvUI)
+
+E:AddTag('Fafnyir_Target', 'UNIT_NAME_UPDATE PLAYER_TARGET_CHANGED UNIT_TARGET', function(unit)
+    local name = UnitName("targettarget")
+    if not name then return "" end
+    return ">> " .. name
+end)
+
+if E.TagInfo then
+    E.TagInfo['Fafnyir_Target'] = { category = "Fafnyir", description = "Displays target of target name with >> prefix" }
+end
